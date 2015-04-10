21927c547b24e05d7d03f278c3a6ab4c15d4e34f
Tykroth <Tykroth2001@gmail.com>
2015-04-10 14:14:28 -0700

    revision 1

--- counter.php
+++ counter.php
@@ -2,24 +2,26 @@
 <html>
 	<body>
 		<?php
-		define("Cnt_num", "CounterNumber.txt");
-		define("LIPS", "LoggedIPS.txt");
-		$current = "1.1.1.4";//$_SERVER["REMOTE_ADDR"];
-		$visitorNumber = file_get_contents(Cnt_num);
-		$previous = file_get_contents(LIPS);
+		define("VISITOR_COUNT_FILE", "CounterNumber.txt");
+		define("IP_LOG_FILE", "LoggedIPS.txt");
+		$current = $_SERVER["REMOTE_ADDR"];
+		$visitorNumber = file_get_contents(VISITOR_COUNT_FILE);
+		$previous = file_get_contents(IP_LOG_FILE);
 		$readablePrevious = unserialize($previous);
 		
-		if(is_array($readablePrevious)){
-			echo "";
-		} else {
-			$readablePrevious=[];
+		
+		//For debugging, must be run once if CounterNumber.txt or LoggedIPS.txt is empty.
+		
+		/*
+		if(!is_array($readablePrevious)){
+			$readablePrevious = [];
 		}
-
 		if(!is_numeric($visitorNumber)) {
-			$visitorNumber = 1;
-		 }
+			$visitorNumber = 0;
+		}
+		*/
 
-		if (!file_exists(Cnt_num) || !file_exists(LIPS)){
+		if (!file_exists(VISITOR_COUNT_FILE) || !file_exists(IP_LOG_FILE)){
 			return "error code 404, file not found.";
 		}
 
@@ -28,15 +30,15 @@
 		} elseif (!in_array($current, $readablePrevious)){
 			array_push($readablePrevious, $current);
 			$serCurrent = serialize($readablePrevious);
-			file_put_contents(LIPS, $serCurrent);
+			file_put_contents(IP_LOG_FILE, $serCurrent);
 
 			$visitorNumber++;
-			file_put_contents(Cnt_num, $visitorNumber);
+			file_put_contents(VISITOR_COUNT_FILE, $visitorNumber);
 
-			echo ("Hello $current! ");
+			echo "Hello $current! ";
 		}
 
-		echo ("You are visitor number $visitorNumber!");
+		echo "You are visitor number $visitorNumber!";
 		?>
 	</body>
 </html>
